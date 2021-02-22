2cae5f43760c302283c865bfbca7207a3b669291
aruncrayons <arunksoman@syncrayons.com>
2021-02-22 16:58:08 +0600

    some test

--- app.py
+++ app.py
@@ -1,27 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 from flask_socketio import SocketIO, join_room
-# import gpxpy
-# import gpxpy.gpx
 from flask_cors import CORS
-# from flask_celery import Celery
 import redis
 
-# Parsing an existing file:
-# -------------------------
-# gpx_file = open('my_gpx_file.gpx', 'r')
-#
-# gpx = gpxpy.parse(gpx_file)
 
 
 app = Flask(__name__)
 r = redis.Redis(host='localhost', port=6379, db=0)
 pubsub = r.pubsub()
 pubsub.subscribe('locations')
-# flask_app.config.update(
-#     CELERY_BROKER_URL='redis://localhost:6379',
-#     CELERY_RESULT_BACKEND='redis://localhost:6379'
-# )
-# celery = make_celery(app)
 
 
 socketio = SocketIO(app,cors_allowed_origins="*")
@@ -33,17 +20,19 @@ def index(username, room):
 
 @socketio.on('join_room')
 def handle_join_room_event(data):
-    # print(data)
+    print(data)
     join_room(data['room'])
     socketio.emit('join_room_announcement', data, room=data['room']);
     return ''
 
-@socketio.on('draw')
+@socketio.on('positions')
 def handle_draw_event(data):
     # join_room(data['room'])
-    socketio.emit('draw_client', data, room=data['room'])
-    # print(data)
+    socketio.emit('mark_position', data, room=data['room'])
+    print(data)
     return ''
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='127.0.0.1')
+
+# uwsgi --http-socket :5000 --plugin python3 --master --wsgi-file app.py --callable app
