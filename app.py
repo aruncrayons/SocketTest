c3edb7485c33ee249bc18c37d234f36d1297d555
aruncrayons <arunksoman@syncrayons.com>
2021-02-23 19:18:52 +0600

    some fixes

--- app.py
+++ app.py
@@ -5,5 +5,4 @@ app = create_app()
 if __name__ == "__main__":
     socketio.run(app, debug=True, host='localhost', port=3000)
 
-### gunicorn --worker-class eventlet -w 1 app:app --bind 0.0.0.0:3000 --reload --threa 10
-0
+### gunicorn --worker-class eventlet -w 1 app:app --bind 0.0.0.0:3000 --reload --threa 100
